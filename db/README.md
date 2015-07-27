26499a10112e14afafd147f938a714bda0d8b726
Ghislain Guiot <gt.ghislain@gmail.com>
2015-07-27 19:03:58 +0200

    add replicat master/slave

---
+++ db/README.md
@@ -0,0 +1,32 @@
+This program has been largely inspired by https://github.com/tutumcloud/mysql.
+
+How to use it
+=============
+
+```sh
+$ docker build -t mysql-softinnov .
+
+$ docker run -d -e REPLICATION_MASTER=true -e REPLICATION_PASS=mypass -P --name master-bearded mysql-softinnov
+
+$ docker exec -it master-bearded mysql -e "CREATE DATABASE $NAME;"
+
+## Example
+$ docker exec -it master-bearded mysql $NAME -e "CREATE TABLE Persons
+(
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255));"
+
+$ docker exec -it master-bearded mysql $NAME -e "INSERT INTO Persons SET PersonID=1, LastName='Me', FirstName='Ni';"
+
+$ docker exec -it master-bearded mysql $NAME -e "SELECT * FROM Persons"
+# Should print 1 row
+
+## First solution with --link
+$ docker run -d -e REPLICATION_SLAVE=true -P --link master-bearded:mysql --name slave-bearded mysql-softinnov
+
+## Second solution with --env-file
+$ docker run -d -e REPLICATION_SLAVE=true -P --env-file=ENV --name slave-bearded mysql-softinnov
+```
--- db/run.sh
+++ db/run.sh
@@ -1,5 +1,115 @@
-#!/bin/sh
+#!/bin/bash
 
-exec mysqld_safe &
+VOLUME_HOME="/var/lib/mysql"
+CONF_FILE="/etc/mysql/my.cnf"
+
+StartMySQL ()
+{
+    /usr/bin/mysqld_safe > /dev/null 2>&1 &
+    # Time out in 1 minute
+    LOOP_LIMIT=60
+    for (( i=0 ; ; i++ )); do
+        if [ ${i} -eq ${LOOP_LIMIT} ]; then
+            echo "Time out. Error log is shown as below:"
+            tail -n 100 ${LOG}
+            exit 1
+        fi
+        echo "=> Waiting for confirmation of MySQL service startup, trying ${i}/${LOOP_LIMIT} ..."
+        sleep 1
+        mysql -uroot -e "status" > /dev/null 2>&1 && break
+    done
+}
+
+OnCreateDB()
+{
+    if [ "$ON_CREATE_DB" = "**False**" ]; then
+        unset ON_CREATE_DB
+    else
+        echo "Creating MySQL database ${ON_CREATE_DB}"
+        mysql -uroot -e "CREATE DATABASE IF NOT EXISTS ${ON_CREATE_DB};"
+        echo "Database created!"
+    fi
+
+}
+
+if [ ${REPLICATION_MASTER} == "**False**"  ]; then
+	unset REPLICATION_MASTER
+fi
+
+if [ ${REPLICATION_SLAVE} == "**False**"  ]; then
+	unset REPLICATION_SLAVE
+fi
+
+# Set MySQL REPLICATION - MASTER
+if [ -n "${REPLICATION_MASTER}" ]; then
+    echo "=> Configuring MySQL replication as master (1/2) ..."
+    if [ ! -f /replication_set.1 ]; then
+        RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
+        echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
+        sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
+        sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
+        touch /replication_set.1
+    else
+        echo "=> MySQL replication master already configured, skip"
+    fi
+fi
+
+# Set MySQL REPLICATION - SLAVE
+if [ -n "${REPLICATION_SLAVE}" ]; then
+    echo "=> Configuring MySQL replication as slave (1/2) ..."
+    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
+        if [ ! -f /replication_set.1 ]; then
+            RAND="$(date +%s | rev | cut -c 1-2)$(echo ${RANDOM})"
+            echo "=> Writting configuration file '${CONF_FILE}' with server-id=${RAND}"
+            sed -i "s/^#server-id.*/server-id = ${RAND}/" ${CONF_FILE}
+            sed -i "s/^#log-bin.*/log-bin = mysql-bin/" ${CONF_FILE}
+            touch /replication_set.1
+        else
+            echo "=> MySQL replication slave already configured, skip"
+        fi
+    else
+        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
+        exit 1
+    fi
+fi
+
+OnCreateDB
+
+echo "=> Starting MySQL..."
+StartMySQL
+
+# Set MySQL REPLICATION - MASTER
+if [ -n "${REPLICATION_MASTER}" ]; then
+    echo "=> Configuring MySQL replication as master (2/2) ..."
+    if [ ! -f /replication_set.2 ]; then
+        echo "=> Creating a log user ${REPLICATION_USER}:${REPLICATION_PASS}"
+        mysql -uroot -e "CREATE USER '${REPLICATION_USER}'@'%' IDENTIFIED BY '${REPLICATION_PASS}'"
+        mysql -uroot -e "GRANT REPLICATION SLAVE ON *.* TO '${REPLICATION_USER}'@'%'"
+        mysql -uroot -e "reset master"
+        echo "=> Done!"
+        touch /replication_set.2
+    else
+        echo "=> MySQL replication master already configured, skip"
+    fi
+fi
+
+# Set MySQL REPLICATION - SLAVE
+if [ -n "${REPLICATION_SLAVE}" ]; then
+    echo "=> Configuring MySQL replication as slave (2/2) ..."
+    if [ -n "${MYSQL_PORT_3306_TCP_ADDR}" ] && [ -n "${MYSQL_PORT_3306_TCP_PORT}" ]; then
+        if [ ! -f /replication_set.2 ]; then
+            echo "=> Setting master connection info on slave"
+            mysql -uroot -e "CHANGE MASTER TO MASTER_HOST='${MYSQL_PORT_3306_TCP_ADDR}',MASTER_USER='${MYSQL_ENV_REPLICATION_USER}',MASTER_PASSWORD='${MYSQL_ENV_REPLICATION_PASS}',MASTER_PORT=${MYSQL_PORT_3306_TCP_PORT}, MASTER_CONNECT_RETRY=30"
+            mysql -uroot -e "start slave"
+            echo "=> Done!"
+            touch /replication_set.2
+        else
+            echo "=> MySQL replication slave already configured, skip"
+        fi
+    else
+        echo "=> Cannot configure slave, please link it to another MySQL container with alias as 'mysql'"
+        exit 1
+    fi
+fi
 
 sql-http-proxy > /var/log/mysql/sql-http-proxy.log 2>&1
